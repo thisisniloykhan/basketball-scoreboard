6f35521f7950d4734bf10e103f040851f7a3377b
Niloy Khan <niloykhan.dev@gmail.com>
2026-05-06 23:54:48 +0600

    refactored code and added css animations

--- script.js
+++ script.js
@@ -4,33 +4,13 @@ let guestScore = document.getElementById("guest-score");
 let homeScoreCount = 0;
 let guestScoreCount = 0;
 
-function home1Increment() {
-  homeScoreCount += 1;
+function increaseHomeScore(increment) {
+  homeScoreCount += increment;
   homeScore.textContent = homeScoreCount;
 }
 
-function home2Increment() {
-  homeScoreCount += 2;
-  homeScore.textContent = homeScoreCount;
-}
-
-function home3Increment() {
-  homeScoreCount += 3;
-  homeScore.textContent = homeScoreCount;
-}
-
-function guest1Increment() {
-  guestScoreCount += 1;
-  guestScore.textContent = guestScoreCount;
-}
-
-function guest2Increment() {
-  guestScoreCount += 2;
-  guestScore.textContent = guestScoreCount;
-}
-
-function guest3Increment() {
-  guestScoreCount += 3;
+function increaseGuestScore(increment) {
+  guestScoreCount += increment;
   guestScore.textContent = guestScoreCount;
 }
 
